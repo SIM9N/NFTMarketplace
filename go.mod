bbce69cd05a63630f9025edc2fa8f3d35a747474
SIM9N <91584856+SIM9N@users.noreply.github.com>
2024-06-01 21:01:41 +0800

    init templ

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Sim9n/nft-marketplace
 
 go 1.22.1
+
+require (
+	github.com/a-h/templ v0.2.707 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+
+	"log"
+	"net/http"
+
+	"github.com/Sim9n/nft-marketplace/view"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Print("Hello World");
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	port := os.Getenv("LISTEN_ADDRESS")
+	
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		view.Index().Render(context.Background(), w)
+	})
+
+	log.Printf("Listening on %v", port)
+	log.Fatal(http.ListenAndServe(port, mux))
 }
